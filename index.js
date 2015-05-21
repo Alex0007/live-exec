e2c6acdbe0971a1a286100ba3da4c22fb03707d7
Alex0007 <Alex0007lolpvp@gmail.com>
2015-05-22 00:03:14 +0500

    env fix

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+var extend = require('lodash/object/extend')
+
 module.exports = function() {
+
+    if (typeof arguments[1] === 'object') { // extend provided env with process.env
+        arguments[1].env = extend(arguments[1].env, process.env)
+    }
+
     var child = require('child_process').exec.apply(null, arguments)
     child.stdout.pipe(process.stdout)
     child.stderr.pipe(process.stderr)
