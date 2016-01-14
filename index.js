6a74cbd171405c152a0624c2a8b8e94cf84bba22
Alex0007 <Alex0007lolpvp@gmail.com>
2016-01-14 15:12:01 +0500

    return child_process instance

--- index.js
+++ index.js
@@ -9,4 +9,6 @@ module.exports = function() {
     var child = require('child_process').exec.apply(null, arguments)
     child.stdout.pipe(process.stdout)
     child.stderr.pipe(process.stderr)
+
+    return child
 }
